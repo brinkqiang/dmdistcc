b5f47806e62147878011139a72ddf20f16391df6
brinkqiang <brink.qiang@gmail.com>
2019-01-23 17:24:26 +0800

    add

--- run-distccd.sh
+++ run-distccd.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [[ "$1" == "" ]] ; then
-        lan_ip=`ip addr | grep inet | grep -v inet6 | grep -v 127.0.0.1 | awk '{print $2}' | awk -F '/' '{print $1}'`
-        echo "exec distccd --daemon --log-stderr --no-detach --allow  $lan_ip"
-        distccd --daemon --log-stderr --no-detach --allow $lan_ip
+        echo "exec distccd --daemon --log-stderr --no-detach --allow 0.0.0.0/0"
+        distccd --daemon --log-stderr --no-detach --allow 0.0.0.0/0
         exit 0
 fi
 
